655e3958f8d0f5e4657ded78b6c79010b9076e58
glarrain <glarrain@example.com>
2013-03-08 19:23:33 -0300

    added basic and canonical 'setup' script

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+setup(
+    name='django-split-testing',
+    version='0.1.0',
+    packages=[''],
+    url='https://github.com/lookup/django-split-testing',
+    license='MIT',
+    author='German Larrain',
+    author_email='glarrain@example.com',
+    description='Intended to help split testing in Django projects'
+)
